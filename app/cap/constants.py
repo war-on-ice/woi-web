5c39a7f7cf38e84bbffec4b024b2d731ddd7afc6
Alexandra Mandrycky <amandrycky@gmail.com>
2015-09-26 15:35:08 -0700

    add team logos
    add team logos + the start of a cap constants.py file.

---
+++ app/cap/constants.py
@@ -0,0 +1,8 @@
+## PLAYER STATUS
+PLAYER_STATUS_GROUP = {"Major": "Roster",
+                       "Minor": "Non-Roster",
+                       "Injured": "Roster",
+                       "Injured Reserve": "Roster",
+                       "Non-Roster": "Non-Roster",
+                       "Suspended": "Roster"}
+
