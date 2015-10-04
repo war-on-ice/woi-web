735eea09fb290e47e1a1011196f3d320fbf75721
Kevin <kan.kevin@gmail.com>
2015-10-04 00:42:34 -0400

    Add responsive tabs for game summary content

--- app/constants.py
+++ app/constants.py
@@ -3,20 +3,20 @@ def create_dict(dictionary):
                     'default': filter(lambda item: item[1]['default'] == 1, dictionary.items())[0][0]}
 
 # Score Situations
-score_situations_dict = {"Trailing By 3+": {'value': 0, 'order': 5, 'default': 0},
-                    "Trailing By 2": {'value': 1, 'order': 7, 'default': 0},
-                    "Trailing By 1": {'value': 2, 'order': 8, 'default': 0},
+score_situations_dict = {"Trailing by 3+": {'value': 0, 'order': 5, 'default': 0},
+                    "Trailing by 2": {'value': 1, 'order': 7, 'default': 0},
+                    "Trailing by 1": {'value': 2, 'order': 8, 'default': 0},
                     "Tied": {'value': 3, 'order': 9, 'default': 0},
-                    "Leading By 1": {'value': 4, 'order': 11, 'default': 0},
-                    "Leading By 2": {'value': 5, 'order':12, 'default': 0},
-                    "Leading By 3+": {'value': 6, 'order': 14, 'default': 0},
+                    "Leading by 1": {'value': 4, 'order': 11, 'default': 0},
+                    "Leading by 2": {'value': 5, 'order':12, 'default': 0},
+                    "Leading by 3+": {'value': 6, 'order': 14, 'default': 0},
                     "All": {'value': 7, 'order': 0, 'default': 1},
-                    "All, Score Adjusted": {'value': 8, 'order': 1, 'default': 0},
+                    "All, score adjusted": {'value': 8, 'order': 1, 'default': 0},
                     "Within 1": {'value': 9, 'order': 3, 'default': 0},
                     "Close": {'value': 10, 'order': 2, 'default': 0},
                     "Leading": {'value': 11, 'order': 10, 'default': 0},
-                    "Leading By 2+": {'value': 12, 'order': 13, 'default': 0},
-                    "Trailing By 2+": {'value': 13, 'order': 6, 'default': 0},
+                    "Leading by 2+": {'value': 12, 'order': 13, 'default': 0},
+                    "Trailing by 2+": {'value': 13, 'order': 6, 'default': 0},
                     "Trailing": {'value': 14, 'order': 4, 'default': 0}}
 
 score_situations = create_dict(score_situations_dict)
@@ -24,12 +24,12 @@ score_situations = create_dict(score_situations_dict)
 
 # Strength Situations
 strength_situations_dict = {"All": {'value': 7, 'order': 2, 'default': 0},
-                      "Even Strength 5v5": {'value': 1, 'order': 1, 'default': 1},
-                      "Power Play": {'value': 2, 'order': 3, 'default': 0},
+                      "Even strength 5v5": {'value': 1, 'order': 1, 'default': 1},
+                      "Power play": {'value': 2, 'order': 3, 'default': 0},
                       "Shorthanded": {'value': 3, 'order': 4, 'default': 0},
                       "4v4": {'value': 4, 'order': 5, 'default': 0},
-                      "Opposing Goalie Pulled": {'value': 5, 'order': 6, 'default': 0},
-                      "Team Goalie Pulled": {'value': 6, 'order': 7, 'default': 0},
+                      "Opposing goalie pulled": {'value': 5, 'order': 6, 'default': 0},
+                      "Team goalie pulled": {'value': 6, 'order': 7, 'default': 0},
                       "Leftovers": {'value': 0, 'order': 8, 'default': 0}}
 
 strength_situations = create_dict(strength_situations_dict)
@@ -56,12 +56,12 @@ all_periods = [1,2,3,4,5,6,7] #assumes max 4 OTs
 periods_options = {"1": {'value': filter(lambda pd: pd == 1, all_periods), 'order': 2, 'default': 0},
            "2": {'value': filter(lambda pd: pd == 2, all_periods), 'order': 3, 'default': 0},
            "3": {'value': filter(lambda pd: pd == 3, all_periods), 'order': 4, 'default': 0},
-           "Regulation": {'value': filter(lambda pd: pd <= 3, all_periods), 'order': 6, 'default': 0},
-           "Overtime": {'value': filter(lambda pd: pd >= 4, all_periods), 'order': 5, 'default': 0},
-           "1 OT": {'value': filter(lambda pd: pd == 4, all_periods), 'order': 7, 'default': 0},
-           "2 OT": {'value': filter(lambda pd: pd == 5, all_periods), 'order': 8, 'default': 0},
-           "3 OT": {'value': filter(lambda pd: pd == 6, all_periods), 'order': 9, 'default': 0},
-           "4 OT": {'value': filter(lambda pd: pd == 7, all_periods), 'order': 10, 'default': 0},
+           "Regulation": {'value': filter(lambda pd: pd <= 3, all_periods), 'order': 5, 'default': 0},
+           "Overtime": {'value': filter(lambda pd: pd >= 4, all_periods), 'order': 6, 'default': 0},
+           "1st OT": {'value': filter(lambda pd: pd == 4, all_periods), 'order': 7, 'default': 0},
+           "2nd OT": {'value': filter(lambda pd: pd == 5, all_periods), 'order': 8, 'default': 0},
+           "3rd OT": {'value': filter(lambda pd: pd == 6, all_periods), 'order': 9, 'default': 0},
+           "4th OT": {'value': filter(lambda pd: pd == 7, all_periods), 'order': 10, 'default': 0},
            "All": {'value': all_periods, 'order': 1, 'default': 1}}
 
 periods = create_dict(periods_options)
